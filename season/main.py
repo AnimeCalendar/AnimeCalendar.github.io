508fd14fcc97a9a265e490e7d21c87575fb99f79
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2026-07-27 19:37:38 -0700

    chore: 26.2.10

--- season/main.py
+++ season/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from json import dump
 from os import popen
 
@@ -19,7 +21,7 @@ def main():
         options=options,
     )
     driver.maximize_window()
-    driver.get("https://animeschedule.net/seasons/summer-2026")
+    driver.get("https://animeschedule.net/seasons/fall-2026")
     shows = {}
     show: WebElement
     for show in driver.find_element(By.CLASS_NAME, "shows-container").find_elements(
--- ts/main.ts
+++ ts/main.ts
@@ -7,7 +7,7 @@
 document.getElementById('list')!.innerHTML =
   localStorage.getItem('list') ?? 'Your List';
 
-const CAL = new Calendar('26.2.9');
+const CAL = new Calendar('26.2.10');
 const SON = new Season();
 
 window.onload = () => {
